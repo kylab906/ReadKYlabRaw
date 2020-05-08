b1a6f51761da493d4780434972e7d0b4d444debf
CHWu <tarsiertail@gmail.com>
2020-05-08 17:03:45 +0800

    add resolution decode, remove resolution parameter

--- dataDecode.py
+++ dataDecode.py
@@ -10,7 +10,44 @@ import math
 from datetime import datetime,timedelta
 
 class dataDecode:
-    def rawdataDecode(rawtxt,res):
+    def HeaderDataDecode(rawtxt):
+        Raw_Data = rawtxt
+        header = Raw_Data[0:512]             #RAW files header
+        #RAW = RAW[512:len(RAW)]         #RAW data
+        
+        header2 = []                    #RAW files header to String
+        for s in header:
+            header2.append(chr(s))
+        
+        
+        time=Raw_Data[320:324]
+
+        timeformat='%Y-%m-%d %H:%M:%S'
+        timestr=(datetime.strptime("2000-01-01 00:00:00", timeformat)+timedelta(seconds=int.from_bytes(time, byteorder='little'))).strftime(timeformat)
+        #%% Acquisition sampling rate ratio
+            
+        splr = header2[39:54]   
+        splr2 = ''.join(splr)
+        splr2 = float(splr2)        #Sampling Rate
+        
+        start = 55
+        SRn = []
+        for i in range(header[36]):     #Acquisition sampling rate ratio SRn
+            SRtemp = header2[start+i*15+i:start+(i+1)*15+i]
+            splrtemp = ''.join(SRtemp)
+            splr_float = float(splrtemp)
+            SRn.append(splr2/splr_float)
+        
+        maxi = int(max(SRn))
+        
+        SR_array=[]
+        for i in range(len(SRn)):
+            SR_array.append(splr2/SRn[i])
+            
+        return SR_array, timestr
+                    
+        
+    def rawdataDecode(rawtxt):
     
         Raw_Data = rawtxt
 
@@ -27,6 +64,11 @@ class dataDecode:
 
         timeformat='%Y-%m-%d %H:%M:%S'
         timestr=(datetime.strptime("2000-01-01 00:00:00", timeformat)+timedelta(seconds=int.from_bytes(time, byteorder='little'))).strftime(timeformat)
+        
+        if header[12]==33:
+            res=2 
+        elif header[12]==35:
+            res=1
         #%% Acquisition sampling rate ratio
             
         splr = header2[39:54]   
@@ -71,7 +113,12 @@ class dataDecode:
         if res==2:
             for i in range(0, len(Raw_Data), len(channel)*2):
                 for j in range(len(channel)):
-                    Data[channel[j]].append(Raw_Data[i + 2*j+1]*256+Raw_Data[i + 2*j])
+                    try:
+                        Data[channel[j]].append(Raw_Data[i + 2*j+1]*256+Raw_Data[i + 2*j])
+                    except IndexError:
+                        pass
+                    except:
+                        print('unknown error')
         elif res==1:
             for i in range(0, len(Raw_Data), len(channel)):
                 for j in range(len(channel)):
